04a0fe464ced5ed861cf1668bffa126961502225
S MIRACLE <91447091+smiracleofficial@users.noreply.github.com>
2021-11-27 15:09:38 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -14,7 +14,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.GL}, (async (message
     await message.sendMessage(Buffer(rashi.data), MessageType.image, {quoted: message.data , thumbnail: fs.readFileSync('./image/r.jpg'), mimetype: Mimetype.png, caption: `*≈≈≈≈≈≈≈Links ☟︎︎︎≈≈≈≈≈≈≈≈*
  
 
-        *Github link        _https://tinyurl.com/yggyjfgf_*
+        *Github link        _https://github.com/smiracleofficial/Zaramwol_*
  
 
        *Audio commads-1     _https://tinyurl.com/yfdkmuhd_*
